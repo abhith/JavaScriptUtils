ae14f6183d561d54aab3a6235cef0236fc1d9111
Abhith Rajan <Abhith@users.noreply.github.com>
2015-10-12 13:05:28 +0530

    added comment

--- src/jQuery/bootstrap-nav-active-selector.js
+++ src/jQuery/bootstrap-nav-active-selector.js
@@ -1,6 +1,7 @@
 /**
  * Created by abhith on 12/10/2015.
  */
+ /*Bootstrap nav active selector*/
 $(document).ready(function () {
     $('ul.nav.navbar-nav').find('a[href="' + location.pathname + '"]')
         .closest('li').addClass('active');
