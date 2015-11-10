fa987c0731372d30efc5c1cfa0c663a77c8658a8
abhith <dearabhith@gmail.com>
2015-11-10 16:15:08 +0530

    Added showFocus directive for angular

---
+++ src/ng/directives/showFocus.js
@@ -0,0 +1,14 @@
+﻿//Focus directive
+appModule.directive('showFocus', function ($timeout) {
+    return function (scope, element, attrs) {
+        scope.$watch(attrs.showFocus,
+          function (newValue) {
+              $timeout(function () {
+                  newValue && element.focus();
+                  if (scope.query && scope.query.length > 0) {
+                      element[0].setSelectionRange(scope.query.length, scope.query.length);
+                  }
+              });
+          }, true);
+    };
+});
