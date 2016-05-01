9b7dac9c7b1ecb4725722ddcc25d744154ce6d9f
Abhith <dearabhith@gmail.com>
2016-05-01 10:53:05 +0400

    star rating

--- src/ng/directives/star-rating/rating.js
+++ src/ng/directives/star-rating/rating.js
@@ -1,31 +1,40 @@
 ﻿(function (module) {
-
     var otfRating = function () {
         return {
             scope: {
-                value: "="
+                value: '=',
+                cid: '=',
+                uid: '=',
+                readonly: '='
             },
-            require: "otfRating",
-            templateUrl: "../templates/rating.html",
-            controller: "otfRatingController",
+            require: 'otfRating',
+            templateUrl: '/app/common/directives/star-rating/rating.html',
+            controller: 'otfRatingController',
             link: function (scope, element, attributes, controller) {
-
-                var min = parseInt(attributes.min || "1");
-                var max = parseInt(attributes.max || "5");
+                var min = parseInt(attributes.min || '1');
+                var max = parseInt(attributes.max || '5');
 
                 controller.initialize(min, max);
             }
         };
     };
 
-    var otfRatingController = function ($scope) {
+    angular
+       .module('app')
+       .controller('otfRatingController', otfRatingController);
 
+    otfRatingController.$inject = ['$scope'];
+
+    function otfRatingController($scope) {
         this.initialize = function (min, max) {
             $scope.preview = -1;
             $scope.stars = new Array(max - min + 1);
         };
 
         $scope.click = function ($index) {
+            if ($scope.readonly) {
+                return;
+            }
             $scope.value = $index + 1;
         };
 
@@ -43,10 +52,6 @@
                 "starpreview": $index <= $scope.preview
             };
         };
-
     };
-
-    module.controller("otfRatingController", otfRatingController);
-    module.directive("otfRating", otfRating);
-
-}(angular.module("app")));
+    module.directive('otfRating', otfRating);
+}(angular.module('app')));
