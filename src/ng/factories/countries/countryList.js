9c48c164e065516295a428d1a378d2bc7d601d06
Risvana <risvana.yusuf15@outlook.com>
2016-05-10 17:02:01 +0530

    country list factory added

---
+++ src/ng/factories/countries/countryList.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('countryList', countryList);
+
+    countryList.$inject = ['$http'];
+
+    function countryList($http) {
+        var service = {
+            getCountries: getCountries
+        };
+
+        return service;
+
+        function getCountries() {
+            // don't forget to assign correct path to the data file below
+            return $http.get('/content/data/countries.json');
+        }
+    }
+})();
