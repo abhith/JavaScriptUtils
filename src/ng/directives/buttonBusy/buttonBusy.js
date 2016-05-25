eca67f5e5c6a107dc63816100fdbd108fc506a4e
Abhith Rajan <Abhith@users.noreply.github.com>
2016-05-25 11:57:24 +0400

    Update buttonBusy.js

--- src/ng/directives/buttonBusy/buttonBusy.js
+++ src/ng/directives/buttonBusy/buttonBusy.js
@@ -1,56 +1,64 @@
-﻿(function () {
-    appModule.directive('buttonBusy', function () {
-          return {
-              restrict: 'A',
-              scope: {
-                  buttonBusy: '='
-              },
-              link: function ($scope, element, attrs) {
+(function () {
+    'use strict';
 
-                  var disabledBefore = false;
-                  var $button = $(element);
-                  var $buttonInnerSpan = $button.find('span');
-                  var buttonOriginalText = null;
-                  
-                  var $icon = $button.find('i');
-                  var iconOriginalClasses = null;
+    angular
+        .module('app')
+        .directive('buttonBusy', buttonBusy);
 
-                  $scope.$watch('buttonBusy', function () {
-                      if ($scope.buttonBusy) {
-                          //disable button
-                          $button.attr('disabled', 'disabled');
-                          //change icon
-                          if ($icon.length) {
-                              iconOriginalClasses = $icon.attr('class');
-                              $icon.removeClass();
-                              $icon.addClass('fa fa-spin fa-spinner');
-                          }
-                          //change text
-                          if (attrs.busyText && $buttonInnerSpan.length) {
-                              buttonOriginalText = $buttonInnerSpan.html();
-                              $buttonInnerSpan.html(attrs.busyText);
-                          }
+    function buttonBusy() {
+        var directive = {
+            link: link,
+            restrict: 'A',
+            scope: {
+                buttonBusy: '='
+            }
+        };
+        return directive;
 
-                          disabledBefore = true;
-                      } else {
-                          if (!disabledBefore) {
-                              return;
-                          }
+        function link($scope, element, attrs) {
+            var disabledBefore = false;
+            var $button = $(element);
+            var $buttonInnerSpan = $button.find('span');
+            var buttonOriginalText = null;
 
-                          //enable button
-                          $button.removeAttr('disabled');
-                          //restore icon
-                          if ($icon.length && iconOriginalClasses) {
-                              $icon.removeClass();
-                              $icon.addClass(iconOriginalClasses);
-                          }
-                          //restore text
-                          if ($buttonInnerSpan.length && buttonOriginalText) {
-                              $buttonInnerSpan.html(buttonOriginalText);
-                          }
-                      }
-                  });
-              }
-          };
-      });
+            var $icon = $button.find('i');
+            var iconOriginalClasses = null;
+
+            $scope.$watch('buttonBusy', function () {
+                if ($scope.buttonBusy) {
+                    //disable button
+                    $button.attr('disabled', 'disabled');
+                    //change icon
+                    if ($icon.length) {
+                        iconOriginalClasses = $icon.attr('class');
+                        $icon.removeClass();
+                        $icon.addClass('fa fa-spin fa-spinner');
+                    }
+                    //change text
+                    if (attrs.busyText && $buttonInnerSpan.length) {
+                        buttonOriginalText = $buttonInnerSpan.html();
+                        $buttonInnerSpan.html(attrs.busyText);
+                    }
+
+                    disabledBefore = true;
+                } else {
+                    if (!disabledBefore) {
+                        return;
+                    }
+
+                    //enable button
+                    $button.removeAttr('disabled');
+                    //restore icon
+                    if ($icon.length && iconOriginalClasses) {
+                        $icon.removeClass();
+                        $icon.addClass(iconOriginalClasses);
+                    }
+                    //restore text
+                    if ($buttonInnerSpan.length && buttonOriginalText) {
+                        $buttonInnerSpan.html(buttonOriginalText);
+                    }
+                }
+            });
+        }
+    }
 })();
